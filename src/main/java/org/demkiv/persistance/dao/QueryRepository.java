cc3e5e9eb6d9c649214bf13edb557d52c83dbd39
Demkiv, Roman [PS] <roman.demkiv@cengage.com>
2024-01-02 16:24:36 +0200

    one more requirments

--- src/main/java/org/demkiv/persistance/dao/QueryRepository.java
+++ src/main/java/org/demkiv/persistance/dao/QueryRepository.java
@@ -1,16 +1,12 @@
 package org.demkiv.persistance.dao;
 
 import lombok.RequiredArgsConstructor;
-import org.demkiv.persistance.entity.Person;
-import org.demkiv.persistance.mapper.PersonRowMapper;
 import org.demkiv.persistance.service.ConverterService;
 import org.demkiv.web.model.PersonFoundForm;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;;
 import org.springframework.stereotype.Repository;
-
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,13 +14,11 @@ public class QueryRepository {
     private final JdbcTemplate jdbcTemplate;
     private final ConverterService converter;
 
-    private final static String PERSONS_SEARCH = "select * from person where FULLNAME like '%?%' or DESCRIPTION like '%?%'";
 
     public List<PersonFoundForm> findPersons(String fullName, String description) {
-        List<Person> persons = Collections.singletonList(jdbcTemplate.queryForObject(PERSONS_SEARCH, new PersonRowMapper(), fullName, description));
-        return persons.stream()
-                .map(converter::convertPersonToPersonFoundForm)
-                .collect(Collectors.toList());
+        String sql = "select * from person where fullname like '%" + fullName + "%' or description like '%" + description + "%'";
+        List<Map<String, Object>> w = jdbcTemplate.queryForList(sql);
+        return null;
     }
 
 }
