8ed35ab013cd09fc3c1532af711579bed9dd35a5
Demkiv, Roman [PS] <roman.demkiv@cengage.com>
2025-03-09 20:47:20 +0200

    add Finder service

--- src/main/java/org/demkiv/persistance/entity/PersonStatus.java
+++ src/main/java/org/demkiv/persistance/entity/PersonStatus.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "")
+@Table(name = "person_status")
 public class PersonStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
